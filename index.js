53fad7525661474b8dfc1364456cfe41dac8073c
takidilmi <angeldilmi@gmail.com>
2023-08-11 19:51:06 +0200

    Compelted assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+  const buttonElement = document.getElementById('button');
+  const inputElement = document.getElementById('main');
+  const divElementWithNumber3 = document.querySelector('div > div > div');
+
+  function clickAlert() {
+    alert('Element clicked!');
+  }
+
+  buttonElement.addEventListener('click', clickAlert);
+  inputElement.addEventListener('click', clickAlert);
+  divElementWithNumber3.addEventListener('click', clickAlert);
+    
 }
+addingEventListener();
